27703807058c77a81b770a780adaa2c3467c477c
Robin <robinsk8a@gmail.com>
2025-03-03 18:07:10 -0500

    Yellow Outline section corrected to fit the new synchrony format with arrows and all the extra and diferent information

---
+++ src/routes/synchrony/+page.svelte
@@ -0,0 +1,531 @@
+<script>
+  const synchUrlCode = '/mmc/NS225154900';
+  const synchImagesLink = 'https://cdn.nmg-platform.com/knightsmattressfurniture/synchrony';
+  const organizationName = 'Knight\'s Mattress & Furniture';
+  const locationSlug = '/ramona';
+</script>
+
+<article class="financing-section">
+	<!-- Fist image container having 2 imaage, one bor desktop and one for mobile -->
+	<section class="financing-banner">
+		<!-- Link for the financing page asociated -->
+		<a href="https://www.mysynchrony.com{synchUrlCode}" target="_blank">
+			<!-- Desktop image -->
+			<!-- IMPORTANT change the src of the credit card image for the one on your site's media assets This is the desktop version which is the fist banner that ussually is used on the slider banner too -->
+			<img class="f-desktop" src="{synchImagesLink}/landing_heroSYNC.webp" />
+			<!-- mobile image -->
+			<!-- IMPORTANT change the src of the credit card image for the one on your site's media assets This is the mobile version which is has a vertical aligned information -->
+			<img
+				class="f-mobile"
+				src="{synchImagesLink}/landing_hero_mobile.webp"
+				alt="Promotional financing banner"
+			/>
+		</a>
+	</section>
+	<!-- Second image container, don't have any link asociated, is just stetically here but it also contains image versions for desktop and mobile -->
+	<section class="credit-card-banner">
+		<!-- Desktop image -->
+		<!-- IMPORTANT change the src of the credit card image for the one on your site's media assets This is the desktop version which is the one that fits across the screen -->
+		<img
+			class="f-desktop"
+			src="{synchImagesLink}/below_heroSYNC.webp"
+			alt="Synchrony credit card accepted banner"
+		/>
+		<!-- Mobile image -->
+		<!-- IMPORTANT change the src of the credit card image for the one on your site's media assets This is the mobile version which is more square -->
+		<img
+			class="f-mobile"
+			src="{synchImagesLink}/below_hero_mobile.webp"
+			alt="Synchrony credit card accepted banner"
+		/>
+	</section>
+	<!-- This is the main container where you will find the big block displaying the 12-60 in yellow outline and white background and the 6 in blue -->
+	<section class="synchrony-info yellow-outline">
+		<!-- Top image whith Synchrony home text -->
+		<div class="info__top-img">
+			<!-- IMPORTANT change the src of the credit card image for the one on your site's media assets This is used both for desktop and mobile and is the little Synchrony Home image displayed on top of the big numbers containers -->
+			<img src="{synchImagesLink}/synchrony-home-thumbSYNC.webp" alt="Synchrony home image" />
+		</div>
+		<!-- Container with that organizes yellow otline and blue background blocks with its headers in a grid layout -->
+    <h3 class="info-header">
+      <span class="bold-blue">PAY OVER TIME WITH PROMOTIONAL FINANCING</span><br />
+      WITH THE SYNCHRONY HOME CREDIT CARD.
+    </h3>
+		<div class="info__container">
+			<!-- Yellow outline header block -->
+
+			<!-- Yellow outline block with the 12-60 months and the promotional financing text -->
+			<div class="info-content text-blue blue-outline">
+				<h2 class="content__l-number text-blue">6</h2>
+				<h2 class="content__months text-blue">MONTHS<sup>1</sup></h2>
+				<h5 class="content__promo-financing text-blue">Promotional Financing</h5>
+				<p class="content__text">
+					on purchases of $299-$1,998.99
+				</p>
+			</div>
+
+      <!-- Synchrony Arrows images -->
+
+      <img class="synchrony-arrows" src="{synchImagesLink}/sync-arrows.webp" alt="Synchrony arrows" />
+
+
+			<!-- Blue background header block -->
+			<!-- Blue background block with the 6 months and the promotional financing text -->
+			<div class="info-content yellow-outline">
+				<h2 class="content__l-number text-yellow">12</h2>
+				<h2 class="content__months text-yellow">MONTHS<sup>1</sup></h2>
+				<h5 class="content__promo-financing text-yellow">Promotional Financing</h5>
+				<p class="content__text">
+					on purchases of $1,999 or more
+				</p>
+			</div>
+		</div>
+	</section>
+	<!-- container for Benefits of the Synchrony section with the credit card image at the bottom -->
+	<section class="synchrony-benefits">
+		<!-- IMPORTANT change the src of the credit card image for the one on your site's media assets. This is the mobile version so it would be the one with just the credit card -->
+		<img class="f-mobile" src="{synchImagesLink}/card.webp" alt="Synchrony card image" />
+		<h1 class="benefits__header">
+			BENEFITS OF THE <span class="bold-blue"
+				>SYNCHRONY HOME<sup>&trade;</sup><br />
+				CREDIT CARD</span
+			>
+		</h1>
+		<ul class="benefits__list two-columns">
+			<li>
+				6 Months Promotional Financing on purchases of 299 or more<sup>1</sup>
+			</li>
+			<li>
+				12 to 60 Months Promotional Financing on qualifying purchases at select Synchrony HOME
+				locations<sup>1</sup>
+			</li>
+			<li>Online account management and customer service</li>
+			<li>
+				A dedicated credit limit for home purchases from furniture and d&eacute;cor to appliances
+				and more, with buying power to help you get what you want and need for your home*
+			</li>
+			<li>
+				No annual fee 2 and the ability to request a credit limit increase<sup>3</sup>
+			</li>
+			<li>0 fraud liability and 24/7 secure account management</li>
+		</ul>
+		<!-- IMPORTANT change the src of the credit card image for the one on your site's media assets This is the desktop version so it would be the one with the card at the left side and a big line accross the width -->
+		<img
+			class="f-desktop"
+			src="{synchImagesLink}/card-imageSYNC.webp"
+			alt="Synchrony card image"
+		/>
+	</section>
+	<!-- Section with text in grey at the bottom of the benefits of the Synchrony element -->
+	<div class="credit-approval">
+		<p>*Subject to credit approval.</p>
+		<p>
+			<sup>1</sup> Subject to credit approval. Minimum monthly payments required. Promotional financing
+			offers available at the time of purchase may vary by location. See store for details.
+		</p>
+		<p>
+			<sup>2</sup> For new accounts: Purchase APR is 29.99%; Minimum Interest Charge is 2. Existing
+			cardholders should see their cardholder agreements for their applicable terms.
+		</p>
+		<p>
+			<sup>3</sup> You may be required to provide your annual net income if we do not have your recent
+			information on file.
+		</p>
+	</div>
+	<!-- Section in blue background text and c2 in white and Synchrony word in yellos bold text -->
+	<div class="synchrony-manage-account blue-background">
+		<div class="manage-account__container">
+			<div class="manage-account__info">
+				<p>
+					MANAGE YOUR ACCOUNT WITH <span class="yellow-bold">SYNCHRONY</span>
+				</p>
+				<p>
+					Make payments, view your balance, update account information and schedule up to 12
+					payments at a time.
+				</p>
+			</div>
+			<div class="manage-account__c2a">
+				<!-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+				<!-- VERY IMPORTANT Change this link for the one on your page, it will look the same but the code at the end varys -->
+				<!-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+				<a href="https://www.mysynchrony.com{synchUrlCode}" target="_blank">APPLY NOW</a>
+			</div>
+		</div>
+	</div>
+	<!-- Last section at the bottom with all the light blue blocks -->
+	<div class="synchrony-qa">
+		<!-- 1 -->
+		<div class="light-blue-background">
+			<h3 class="bold-blue">
+				What is the {organizationName} Synchrony HOME<sup>&trade;</sup> Credit Card?
+			</h3>
+			<p>
+				{organizationName} offer customers a credit card through Synchrony Bank with a revolving credit
+				limit to use again and again everywhere the Synchrony HOME Credit Card is accepted, including
+				{organizationName}
+				locations.*
+			</p>
+			<small>*Subject to credit approval.</small>
+		</div>
+		<!-- 2 -->
+		<div class="light-blue-background">
+			<h3 class="bold-blue">
+				How do I apply for a {organizationName} Synchrony HOME<sup>&trade;</sup> Credit Card?
+			</h3>
+			<p>
+				You can apply for a {organizationName} Synchrony HOME Credit Card by applying online or at
+				any Synchrony HOME retailer location. Click
+				<!-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+				<!-- VERY IMPORTANT Change the location for your store's location -->
+				<!-- ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
+				<a href="/locations{locationSlug}"
+					>here to apply online or here to find your nearest participating store location</a
+				>
+			</p>
+		</div>
+		<!-- 3 -->
+		<div class="light-blue-background">
+			<h3 class="bold-blue">How do I manage my account online?</h3>
+			<p>
+				To manage your {organizationName} Synchrony HOME Credit Card account online, click on the Manage
+				My Account link above/below to go to the Synchrony Bank website. To logging in to your account,
+				you will be able to:
+			</p>
+			<ul class="two-columns">
+				<li>Make a payment.</li>
+				<li>View your balance.</li>
+				<li>Update your account information.</li>
+				<li>Schedule up to 12 future payments.</li>
+			</ul>
+		</div>
+		<!-- 4 -->
+		<div class="light-blue-background">
+			<h3 class="bold-blue">
+				How can I make a payment on my {organizationName} Synchrony HOME<sup>&trade;</sup> Credit Card?
+			</h3>
+			<p>There are three convenient ways to pay your bill.</p>
+			<div class="two-columns">
+				<p>
+					<strong>Pay Online:</strong>
+					<!-- IMPORTANT verify this link but it usully the same one -->
+					<a href="https://www.mysynchrony.com/mysyf/cmp-index.html">Click here</a> to manage your account
+					and make payments online.
+				</p>
+				<p>
+					<strong>Pay by Phone:</strong> Contact Synchrony Bank Customer Service 1-844-335-5909 during
+					the hours listed below to make a payment by phone.
+				</p>
+				<p>
+					<em>Monday - Sunday 7:30 am to 12:00 midnight EST</em>
+				</p>
+				<p>
+					<strong>Pay by Mail:</strong> Mail payments to the address below. Ensure that you have plenty
+					of time for your payment to arrive prior to the due date.
+				</p>
+				<p>
+					Synchrony Bank<br />
+					PO Box 960061<br />
+					Orlando, FL 32896-0061
+				</p>
+			</div>
+		</div>
+	</div>
+</article>
+
+<style>
+  /* container styling */
+  .financing-section {
+    font-family: sans-serif;
+    max-width: 1440px;
+    margin-inline: auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+    padding: 3rem;
+  }
+  /* Styles for every a tag or link in the section */
+  .financing-section a{
+    text-decoration: none;
+    color: #0a6282;
+    font-weight: 600;
+  }
+  /* Styles for every a tag or link in the section when hovered */
+  .financing-section a:hover{
+    color: #d6a80e;
+  }
+
+  /* These are the styles for the info main container where displays the 12-60 yellow outline block and the 6 blue background block */
+  .synchrony-info {
+    width: 100%;
+    border-radius: 1.2rem;
+    padding-top: 2.5rem;
+    margin-top: 2.5rem;
+    text-align: center;
+    position: relative;
+  }
+  /* This let the top imge to be on top of the container outline with position absolute */
+  .info__top-img {
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%) translateY(-50%);
+  }
+  /* This class defines the grid layout for the information in the block */
+  .info__container {
+    padding: 2rem 5%;
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: 1fr auto 1fr;
+    grid-template-rows: fit-content(100%);
+  }
+  /* Set a yellow outline around the block */
+  .yellow-outline {
+    outline: #fac40b solid 0.2rem;
+    outline-offset: -0.2rem;
+  }
+  /* Set a blue outline around the block */
+  .blue-outline {
+    outline: #0a6282 solid 0.2rem;
+    outline-offset: -0.2rem;
+  }
+  /* set color yellow for every blue text in the page */
+  .text-yellow {
+    color: #f7b500;
+  }
+  /* set color blue for every blue text in the page */
+  .text-blue {
+    color: #0a6282;
+  }
+  /* Set the background color for the blue block */
+  .blue-background {
+    background-color: #0a6282;
+    color: #fff;
+  }
+  /* Styales for headers section on top of yellow out and blue back blocks */
+  .info-header {
+    font-size: 1.6rem;
+    height: 100%;
+    margin-top: 2rem;
+    margin-bottom: 0.6rem;
+    font-weight: 700;
+    padding-inline: 1rem;
+  }
+  /* This class sets padding, radius and grid position for the big number blocks */
+  .info-content {
+    border-radius: 1.2rem;
+    padding: 2rem 1rem;
+    grid-row: 1;
+  }
+  /* Synchrony Arrows Image */
+  .synchrony-arrows {
+    max-width: 4.5rem;
+    margin-block: auto;
+    grid-column: 2;
+  }
+
+  .info-content.yellow-outline {
+    grid-column: 3;
+  }
+  /* This makes the styles for the large numbers in the blocks */
+  .content__l-number {
+    font-size: 6rem;
+    font-weight: 200;
+    margin: 0;
+  }
+  /* This makes the styles for the months in the blocks */
+  .content__months {
+    font-size: 3.5rem;
+    font-weight: 200;
+    margin: 0;
+  }
+  .content__months sup {
+    font-size: 0.6em;
+    font-weight: 500;
+  }
+  /* This makes the styles for the promotional financing text in the blocks */
+  .content__promo-financing {
+    font-size: 2rem;
+    margin: 0;
+  }
+  /* This makes the styles for the text in the blocks */
+  .content__text {
+    font-size: 2rem;
+    max-width: 20rem;
+    justify-self: auto;
+    margin: 0 auto;
+    color: #000;
+    text-wrap: balance;
+    padding-inline: 1rem;
+    margin-top: 2rem;
+  }
+  /* This are the styles fot the container that starts with Benefits of the Sybrony home */
+  .synchrony-benefits {
+    margin-top: 2rem;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-template-rows: fit-content(100%) 2fr;
+    font-size: 0.9rem;
+  }
+  /* Large lettering in the benefits container */
+  .benefits__header {
+    margin-top: 0;
+    grid-column: 1/2;
+    grid-row: 1/2;
+    font-size: 1.6rem;
+  }
+  /* Set styles for those text with blue bold text */
+  .bold-blue {
+    color: #0a6282;
+    font-weight: 700;
+  }
+  /* These are the items that list the conditions for financing */
+  .benefits__list {
+    grid-column: 2/4;
+    grid-row: 1/3;
+  }
+  /* This class display in two columns the list of benefits */
+  .two-columns {
+    columns: 2; 
+    column-gap: 3em;
+  }
+  /* Set styles for the image in the benefits container */
+  .synchrony-benefits img {
+    grid-column: 1/4;
+    grid-row: 2/3;
+  }
+  /* Set the stiles for the element at the bottom of the benefits section in a grey font */
+  .credit-approval {
+    font-size: 0.8rem;
+    margin-inline: 3rem;
+    color: #777777;
+  }
+  /* Styles for the section with blue background and whit call to action that says "APPLY NOW" */
+  .synchrony-manage-account {
+    width: 100%;
+    margin-top: 2rem;
+    margin-bottom: 3rem;
+  }
+  .manage-account__container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+    padding: 2rem 4rem;
+    margin: auto;
+  }
+  .manage-account__info {
+    max-width: 40rem;
+    margin-right: 2rem;
+    display: block;
+  }
+  .manage-account__info p:last-child {
+    margin: 0;
+  }
+  .yellow-bold {
+    font-weight: 700;
+    color: #fac40b;
+  }
+  /* Styles for the call to action in the Manage block */
+  .manage-account__c2a a{
+    display: block;
+    width: max-content;
+    text-align: center;
+    border: solid 0.2rem #fff;
+    padding: 0.5rem 1rem;
+    color: #fff;
+    font-weight: 500;
+  }
+  /* hover Styles for the call to action in the Manage block */
+  .manage-account__c2a a:hover {
+    background-color: #fff;
+    color: #0a6282;
+  }
+  /* This class set the light blue background for every item in the qa section at the bottom oth the page */
+  .light-blue-background {
+    background-color: #deedef;
+    padding: 2rem;
+    border-radius: 1rem;
+    margin-bottom: 2rem;
+  }
+  .f-desktop {
+    width: 100%;
+    display: block;
+  }
+  .f-mobile {
+    display: none;
+  }
+  @media (max-width: 1000px) {
+    .financing-section {
+      gap: 3rem;
+      padding: 0.2rem;
+    }
+    .f-desktop {
+      display: none;
+    }
+    .f-mobile {
+      display: block;
+      width: 100%;
+    }
+    .info__container {
+      grid-template-columns: 1fr;
+      grid-auto-flow: column;
+    }
+    
+    .info-header {
+      grid-column: 1/2;
+      grid-row: auto;
+      font-size: clamp(1.2rem, 5vw, 2rem);
+    }
+    .content__months {
+      font-size: clamp(1.5rem, 5vw, 3rem);
+    }
+    .content__promo-financing {
+      font-size: clamp(1.5rem, 5vw, 3rem);
+    }
+    .content__text {
+      font-size: clamp(1.5rem, 5vw, 3rem);
+    }
+    .info-content {
+      grid-column: 1/2;
+      grid-row: auto;
+    }
+    .synchrony-arrows {
+      display: none;
+    }
+    .info-content.yellow-outline {
+      grid-column: 1;
+    }
+    .synchrony-benefits {
+      grid-template-columns: 1fr;
+      grid-template-rows: fit-content(100%);
+    }
+    .benefits__header {
+      grid-column: 1/2;
+      grid-row: auto;
+    }
+    .benefits__list {
+      grid-column: 1/2;
+      grid-row: auto;
+    }
+    .synchrony-benefits img {
+      grid-column: 1/2;
+      grid-row: auto;
+    }
+    .two-columns {
+      columns: 1;
+    }
+    .credit-approval {
+      text-align: center;
+    }
+    .manage-account__container {
+      flex-direction: column;
+      text-align: center;
+      gap: 1.5rem;
+      padding: 4rem;
+    }
+    .manage-account__container > * {
+      margin: 0;
+    }
+  }
+</style>
